a2e57d164c0e0a2141ab966337ff388b9a6066f3
Yuri Beloborodov <yubeloborodov@gmail.com>
2014-10-03 17:47:18 +0600

    Fixes #750, refs #738

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,35 +2,62 @@ class OrdersController < ApplicationController
   unloadable
 
   def import
-    api_token = Setting.plugin_orders['orders_api_key']
-    url = Setting.plugin_orders['orders_api_path']
-    begin
-      response = RestClient.get(url, :params => { :range => params[:order_id], :auth_token => api_token})
-      redirect_to new_project_issue_path options_from_(response)
-    rescue => e
-      render inline: e.response
+    if create_issue_from(order_response)
+      redirect_to edit_issue_path @issue
+    else
+      redirect_to orders_path
     end
   end
 
   private
 
-  def options_from_(response)
-    items = JSON.parse(response)
-    item = items.first
+  def order_response
+    api_token = Setting.plugin_orders['orders_api_key']
+    url = Setting.plugin_orders['orders_api_path']
+    resp = RestClient.get(url, :params => { :range => params[:order_id], :auth_token => api_token})
+  end
 
+  def create_issue_from(resp)
+    items = JSON.parse(resp)
+    item = items.first
+    if item
+      build_issue_from(item)
+      if @issue.save
+        return true
+      else
+        flash[:error] = @issue.errors.empty? ? "Error" : @issues.errors.full_messages.to_sentence
+        return false
+      end
+    else
+      flash[:error] = l(:order_params_error)
+      return false
+    end
+  end
+
+  def build_issue_from(item)
     options = {
-    "project_id" => 16,  
-    "issue[subject]" => item["product_info"]["title"],
-    "issue[custom_field_values][1]"  => item["user"]["email"],
-    "issue[custom_field_values][13]" => item["user"]["contact_name"],
-    "issue[custom_field_values][14]" => item["user"]["phone"],
-    "issue[custom_field_values][6]"  => item["attendees"],
-    "issue[custom_field_values][11]" => item["contractor"]["full_name"],
-    "issue[custom_field_values][22]" => item["contractor"]["name"],
-    "issue[custom_field_values][23]" => item["contractor"]["inn"],
-    "issue[custom_field_values][20]" => item["contractor"]["office_email"],
-    "issue[custom_field_values][26]" => item['contractor']['office_phone'],
-    "issue[custom_field_values][12]" => item["contractor"]["postal_address"]
-    } if item
+      "project_id" => 16,  
+      "subject" => item["product_info"]["title"],
+      "tracker_id" => 4,
+      "author" => User.find_by_login("uri"),
+      "order_info" => item
+      }
+    options.merge!("custom_field_values" => custom_fields(item))
+    @issue = Issue.new(options)
+  end
+
+  def custom_fields(item)
+    cf = {
+      "1"  => item["user"]["email"],
+      "13" => item["user"]["contact_name"],
+      "14" => item["user"]["phone"],
+      "6"  => item["attendees"],
+      "11" => item["contractor"]["full_name"],
+      "22" => item["contractor"]["name"],
+      "23" => item["contractor"]["inn"],
+      "20" => item["contractor"]["office_email"],
+      "26" => item['contractor']['office_phone'],
+      "12" => item["contractor"]["postal_address"]
+    }
   end
 end
