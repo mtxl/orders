ca9a24b3abaa2613da9922b62f9771c800b2c936
Yuri Beloborodov <yubeloborodov@gmail.com>
2014-10-04 21:24:41 +0600

    Remove pathces

--- lib/orders/pathces/issues_helper_patch.rb
+++
@@ -1,7 +0,0 @@
-module IssuesHelper
-  def template_contract_options_for_select(institute)
-    p = Wiki.find_page("#{@project.name}:#{institute}")
-    a = p ? p.children.inject([]) {|a, i| a << [i.title,i.title]} : []
-    options_for_select(a)
-  end
-end
